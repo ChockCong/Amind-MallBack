2edda2e842f86a1ded81b370d6d52ce8993a468b
kc <kc.choi@amind.tech>
2018-05-08 14:30:16 +0800

    first

---
+++ src/components/promotion-add/promotion-add.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { PromotionAddComponent } from './promotion-add';
+
+@NgModule({
+  declarations: [
+    PromotionAddComponent,
+  ],
+  imports: [
+    IonicModule,
+  ],
+  exports: [
+    PromotionAddComponent
+  ]
+})
+export class PromotionAddComponentModule {}
